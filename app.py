45f95ad7e729a0a7d731cf39691478e6912df80b
emmaudeji <gogreneinfo@gmail.com>
2022-04-25 09:55:09 +0100

    my portfolio

---
+++ app.py
@@ -0,0 +1,48 @@
+import email
+from lib2to3.pgen2.token import NEWLINE
+from urllib import request
+from flask import Flask, render_template, request, redirect 
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html') 
+
+@app.route("/<string:page_name>")
+def about(page_name):
+    return render_template(page_name)
+
+def write_to_database(data):
+    with open('database.txt', mode='a') as database:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{email}, {subject}, {message}')
+
+def write_to_csv(data):
+    with open('database.csv', 'a', newline='') as csvfile:
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_csv(data)
+        write_to_database(data)
+        return redirect('/thankyou.html')
+    else:
+        return 'something went wrong'
+
+
+
+
+
+
